fbbc6b3e8689900c73466e281f45b4f21b43f6cd
jallend1 <jallend1@gmail.com>
2020-04-12 16:38:15 -0700

    Adds score functionality and begins implementation of laser removal upon hit

--- app.js
+++ app.js
@@ -21,14 +21,27 @@ function checkGameOver(){
 }
 
 function handleLasers(){
-    cells.forEach(cell => cell.classList.remove('laser'));                          //Removes all existing lasers before updating
+    cells.forEach(cell => cell.classList.remove('laser'));                                              //Removes all existing lasers before updating
     if(lasers.length){                                                              //Makes sure we got lasers to process
         for(let i = 0; i < lasers.length; i++){                                     //Moves every laser to its new position
             lasers[i] -= gameSize;                                      
         }
-        lasers.forEach((laser, index) => laser < 0                                  //Renders lasers and saves off-grid lasers for removal
-            ? deadLasers.push(index) 
-            : cells[laser].classList.add('laser')); 
+        lasers.forEach((laser, index) => {
+            if(laser < 0){
+                deadLasers.push(index) 
+            }                                  //Renders lasers and saves off-grid lasers for removal
+            else if(cells[laser].classList.contains('enemy')){
+                cells[laser].classList.add('laser');
+                cells[laser].classList.add('boom');
+                setTimeout(() => cells[laser].classList.remove('boom'), 250);
+                score++;
+                deadLasers.push(index);
+            }
+            else{
+                cells[laser].classList.add('laser');
+
+            } 
+        }); 
         deadLasers.forEach(dead => {                                                
             lasers.splice(dead, 1);                                                 //Removes every laser that's traveled off-screen from the array
             deadLasers.shift();                                                     //Clears removed lasers from to-remove array
@@ -55,23 +68,33 @@ function generateBaddies(){
             baddies[i] += direction;
         }
     }
-        checkGameOver();
-        baddies.forEach((baddie, index) => {
-            if(cells[baddie].classList.contains('laser')){
-                cells[baddie].classList.add('boom');
-                deadBaddies.push(index)
+    checkGameOver();
+    baddies.forEach((baddie, index) => {
+        if(cells[baddie].classList.contains('laser')){
+            score++;
+            cells[baddie].classList.add('boom');
+            setTimeout(() => cells[baddie].classList.remove('boom'), 250);
+            if(deadBaddies.indexOf(index) === -1){                              // Prevents duplicate numbers from getting pushed
+                deadBaddies.push(index)                                         // Pushes the location of the dead alien to be ignored later
             }
-            else{
-                cells[baddie].classList.add('enemy');
+        }
+        if(deadBaddies.length){                                                 // If there are dead aliens, check the array
+            for(let i = 0; i < baddies.length; i++){
+                if(deadBaddies.indexOf(i) === -1){                              // If alien is not in dead array, paints alien style
+                    cells[baddies[i]].classList.add('enemy');  
+                }
             }
-        });
+        }
+        else{                                                                       // If no dead aliens, paints all aliens
+            cells[baddie].classList.add('enemy'); 
+        }
+    });
 }
 
 function generateBoard(){
     for(let i = 0; i < gameSize * gameSize; i++){
         const newDiv = document.createElement('div');
         newDiv.classList.add('cell');
-        newDiv.textContent = i;
         gameArea.appendChild(newDiv);
     }
     return(document.querySelectorAll('.cell'));
@@ -96,6 +119,7 @@ function update(){
     drawPlayer();
     generateBaddies();
     handleLasers();
+    scoreDisplay.textContent = score;
 }
 
 
